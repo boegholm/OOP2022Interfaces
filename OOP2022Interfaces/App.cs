53a0987272a69ed8abf16ecffb47b7c6b386ad92
Thomas Bøgholm <github.com@glirc.dk>
2022-02-24 10:19:02 +0100

    Add project files.

---
+++ OOP2022Interfaces/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP2022Interfaces
+{
+    class App
+    {
+        public void Run()
+        {
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(new Employee(1, "Thomas"));
+            employees.Add(new Employee(3, "Mads"));
+            employees.Add(new Employee(2, "Henrik"));
+            employees.Add(new Employee(4, "Mogens"));
+            employees.Add(new Employee(0, "Elisabeth"));
+
+            //employees.Sort();
+
+            employees.Sort(new RevEmployeeComparerById());
+
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            employees.Sort(new EmployeeComparerById());
+
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine(GetMax(1,2));
+            Console.WriteLine(GetMin("Thomas", "Hans"));
+
+            PrintWinner(employees[2], employees[3]);
+
+        }
+
+        public object GetMin(IComparable a, IComparable b)
+        {
+            if (a.CompareTo(b) < 0)
+                return a;
+            else
+                return b;
+        }
+
+        public object GetMax(IComparable a, IComparable b)
+        {
+            if (a.CompareTo(b) > 0)
+                return a;
+            else
+                return b;
+
+        }
+
+
+        public void PrintWinner(Employee a, Employee b)
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("Comparing:");
+            Console.WriteLine(a);
+                Console.WriteLine(b);
+            Console.WriteLine("*******************");
+            Console.WriteLine(GetMax(a, b));
+        }
+    }
+}
---
+++ OOP2022Interfaces/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP2022Interfaces
+{
+
+    interface IUSB
+    {
+        void TurnOn();
+    }
+    interface IFirewire
+    {
+        void Bar();
+        void TurnOn();
+    }
+
+    class Employee : IComparable, IComparable<Employee>
+    {
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Employee other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo((Employee)obj);
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}....{Name}]";
+        }
+    }
+
+    class A
+    {
+
+        public void Foo()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    //class B
+    //{
+    //    public void Foo()
+    //    {
+    //        Console.WriteLine("B");
+    //    }
+    //}
+
+    class AHarddisk
+    {
+        public void TurnOn()
+        {
+
+        }
+    }
+
+    class ExternHarddisk : AHarddisk, IUSB,IFirewire
+    {
+
+        public void Bar()
+        {
+            Console.WriteLine("C bar");
+        }
+    }
+
+    
+
+
+    class EmployeeComparerById : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return x.Id.CompareTo(y.Id);
+        }
+    }
+    class RevEmployeeComparerById : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return -1*x.Id.CompareTo(y.Id);
+        }
+    }
+
+    class FancyStringComparer : IComparer<string>
+    {
+        public FancyStringComparer(int startIndex)
+        {
+            StartIndex = startIndex;
+        }
+
+        public int StartIndex { get; }
+
+        public int Compare(string a, string b)
+        {
+            return a.Substring(StartIndex).CompareTo(b.Substring(StartIndex)); 
+        }
+    }
+    class RevStringComparer<T> : IComparer<T>
+    {
+        IComparer<T> other;
+
+        public RevStringComparer(IComparer<T> other)
+        {
+            this.other = other;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return -1*other.Compare(x, y);
+        }
+    }
+
+    class App2
+    {
+        public void Run()
+        {
+            List<string> l = new List<string>() { "a Thomas", "b Hans", "c Anne" };
+            l.Sort(new RevStringComparer<string>(new FancyStringComparer(2)));
+            foreach (var item in l)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //string s = "Thomas";
+            //IComparable c = s;
+
+
+            App2 app = new App2();
+            app.Run();
+
+
+            //Object o = new ExternHarddisk();
+
+            //IUSB ia = new ExternHarddisk();
+            //IFirewire ib = new ExternHarddisk();
+            //ib.Bar();
+            
+            //C c = new ExternHarddisk();
+            //c.Foo();
+        }
+    }
+}
